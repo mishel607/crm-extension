2e3781d58e6f73a2c1edb6dfebc996ba98f71ee4
unknown <imsiddiquee@gmail.com>
2021-04-02 08:51:40 +0600

    add hubspot event

--- 1-routes/routeCollection/ticketRoute.js
+++ 1-routes/routeCollection/ticketRoute.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 
 const {
   get_tickets,
+  get_hubspot_tickets,
   post_ticket,
 } = require("../../3-controllers/ticketController");
 
 router.get("/", get_tickets);
+router.get("/hubspot", get_hubspot_tickets);
 router.post("/", post_ticket);
 
 module.exports = router;
--- 3-controllers/ticketController.js
+++ 3-controllers/ticketController.js
@@ -11,6 +11,61 @@ exports.get_tickets = async (req, res, next) => {
   }
 };
 
+exports.get_hubspot_tickets = async (req, res, next) => {
+  try {
+    const results = await Ticket.find();
+    console.log("get_tickets");
+    //return res.send(results);
+
+    res.json({
+      applicationId: 767676,
+      baseUris: ["https://example.com/actions"],
+      dataFetchUri: "https://example.com/demo-tickets",
+      title: "DemoTickets",
+      propertyDefinitions: [
+        {
+          name: "ticket_type",
+          label: "Ticket type",
+          dataType: "STRING",
+        },
+        {
+          options: [
+            {
+              type: "SUCCESS",
+              label: "In Progress",
+              name: "inprogress",
+            },
+            {
+              type: "DEFAULT",
+              label: "Resolved",
+              name: "resolved",
+            },
+          ],
+          name: "status",
+          label: "Status",
+          dataType: "STATUS",
+        },
+        {
+          name: "priority",
+          label: "Priority",
+          dataType: "STRING",
+        },
+        {
+          name: "project",
+          label: "Project",
+          dataType: "STRING",
+        },
+      ],
+      associatedHubSpotObjectTypes: ["COMPANY"],
+      associatedHubSpotObjectTypeProperties: {
+        COMPANY: ["domain"],
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 exports.post_ticket = async (req, res, next) => {
   const { name, description } = req.body;
   console.log("post_ticket");
