fc53a6906a6507e43dd7ea18b3ec821cdc70b6e6
unknown <imsiddiquee@gmail.com>
2021-04-22 20:09:21 +0600

    implement api post_get_a_group_of_line_items_by_id

--- 1-routes/routeCollection/hubspoRoute.js
+++ 1-routes/routeCollection/hubspoRoute.js
@@ -2,8 +2,13 @@ const router = require("express").Router();
 
 const {
   get_a_line_item_by_id,
+  post_get_a_group_of_line_items_by_id,
 } = require("../../3-controllers/hubspotLineItemController");
 
 router.get("/api/get_a_line_item_by_id/:id", get_a_line_item_by_id);
+router.post(
+  "/api/post_get_a_group_of_line_items_by_id",
+  post_get_a_group_of_line_items_by_id
+);
 
 module.exports = router;
--- 3-controllers/hubspotLineItemController.js
+++ 3-controllers/hubspotLineItemController.js
@@ -2,6 +2,7 @@ var request = require("request-promise");
 const { json } = require("express");
 const { hubs } = require("../5-utils/constants");
 
+//http://localhost:8080/tierone/get_a_line_item_by_id/1259303592?client=TierOne
 exports.get_a_line_item_by_id = async (req, res, next) => {
   try {
     const lineId = req.params.id;
@@ -40,34 +41,56 @@ exports.get_a_line_item_by_id = async (req, res, next) => {
   }
 };
 
-async function get_a_group_of_line_items_by_id(lineId) {
+//http://localhost:8080/hubspot/api/post_get_a_group_of_line_items_by_id?client=TierOne
+//body
+// {
+//   "ids": [
+//     1259303592,
+//     1308941986
+
+//   ]
+// }
+exports.post_get_a_group_of_line_items_by_id = async (req, res, next) => {
   try {
-    const { properties } = await request({
+    const { ids } = req.body;
+    const { client } = req.query;
+
+    console.log("ids::", ids);
+
+    const result = await request({
       method: "POST",
-      url: `https://api.hubapi.com/crm-objects/v1/objects/line_items/batch-read`,
-      qs: {
-        hapikey: "3dea956b-54b7-4f8c-af5b-c5c8d5af3add",
-        properties: "hs_product_id",
-        properties: "name",
-        properties: "supplier_drop_down",
-        properties: "price",
-        properties: "quantity",
+      url: `https://api.hubapi.com/crm-objects/v1/objects/line_items/batch-read?hapikey=${hubs[client].hapikey}&properties=name&properties=quantity&properties=price&properties=hs_product_id&properties=supplier_drop_down`,
+
+      body: {
+        ids,
       },
       json: true,
     });
 
-    let lineItems = {
-      lineId,
-      name: properties.name ? properties.name.value : "",
-      hs_product_id: properties.hs_product_id
-        ? properties.hs_product_id.value
-        : 0,
-      supplier_drop_down: properties.supplier_drop_down
-        ? properties.supplier_drop_down.value
-        : "",
-      price: properties.price ? properties.price.value : 0,
-      quantity: properties.quantity ? properties.quantity.value : 0,
-    };
-    return deal;
-  } catch (error) {}
-}
+    let lineItems = [];
+
+    for (let current of ids) {
+      let properties = result[current].properties;
+
+      let lineItem = {
+        dealId: "",
+        companyId: "",
+        name: properties.name ? properties.name.value : "",
+        product_id: properties.hs_product_id
+          ? properties.hs_product_id.value
+          : 0,
+        carrier: properties.supplier_drop_down
+          ? properties.supplier_drop_down.value
+          : "",
+        price: properties.price ? properties.price.value : 0,
+        quantity: properties.quantity ? properties.quantity.value : 0,
+      };
+
+      lineItems.push(lineItem);
+    }
+
+    return res.json({ lineItems });
+  } catch (error) {
+    console.log(error);
+  }
+};
